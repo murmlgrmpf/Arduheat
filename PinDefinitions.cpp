f8682bc6a421b85f11a2880faef5ddd9975c9ae1
Thomas Emmert <t.3mmert@gmail.com>
2012-11-16 22:31:05 +0100

    updated Pindefinitions

--- PinDefinitions.cpp
+++ PinDefinitions.cpp
@@ -10,9 +10,17 @@ void PinInitialization(void)
   pinMode(MultiplexControl2, OUTPUT);
   pinMode(MultiplexControl3, OUTPUT);
   pinMode(MultiplexControl4, OUTPUT);
-  
+ 
+ // TempHeatControl Initialization
+  pinMode(TempHeatControl, INPUT);
+
+  // Motor Sense Initialization
+  pinMode(PinValveWarmWaterMotorSense, INPUT);
+  pinMode(PinValveBoilerMotorSense, INPUT);
+  pinMode(PinValveSolarMotorSense, INPUT);
+  pinMode(PinValveHeatSource1MotorSense, INPUT);
+
   // Pumps and Valves intialization
-  
   pinMode(PinPumpCirculation, OUTPUT);
   pinMode(PinValveWarmWaterOpen, OUTPUT);
   pinMode(PinValveBoilerOpen, OUTPUT);
@@ -30,17 +38,10 @@ void PinInitialization(void)
   pinMode(PinMixerOpen, OUTPUT);
   pinMode(PinMixerClose, OUTPUT);
   pinMode(PinPulseCounter, INPUT);
-  
+  pinMode(PinHeatControl, OUTPUT);
+
   digitalWrite(PinPumpCirculation, HIGH);
-  digitalWrite(PinValveWarmWaterOpen, HIGH);
-  digitalWrite(PinValveBoilerOpen, HIGH);
-  digitalWrite(PinValveSolarOpen, HIGH);
-  digitalWrite(PinValveHeatSource1Open, HIGH);
   digitalWrite(PinStartHeatSource1, HIGH);
-  digitalWrite(PinValveWarmWaterClose, HIGH);
-  digitalWrite(PinValveBoilerClose, HIGH);
-  digitalWrite(PinValveSolarClose, HIGH);
-  digitalWrite(PinValveHeatSource1Close, HIGH);
   digitalWrite(PinPumpWarmWater, HIGH);
   digitalWrite(PinPumpBoiler, HIGH);
   digitalWrite(PinPumpSolar, HIGH);
@@ -48,5 +49,41 @@ void PinInitialization(void)
   digitalWrite(PinMixerOpen, HIGH);
   digitalWrite(PinMixerClose, HIGH);
   //digitalWrite(PinPulseCounter, HIGH);
-  
+
+  // Room Heating intialization  
+  pinMode(Room1, OUTPUT);
+  pinMode(Room2, OUTPUT);
+  pinMode(Room3, OUTPUT);
+  pinMode(Room4, OUTPUT);
+  pinMode(Room5, OUTPUT);
+  pinMode(Room6, OUTPUT);
+  pinMode(Room7, OUTPUT);
+  pinMode(Room8, OUTPUT);
+  pinMode(Room9, OUTPUT);
+  pinMode(Room10, OUTPUT);
+  pinMode(Room11, OUTPUT);
+  pinMode(Room12, OUTPUT);
+  pinMode(Room13, OUTPUT);
+  pinMode(Room14, OUTPUT);
+  pinMode(Room15, OUTPUT);
+  pinMode(Room16, OUTPUT);
+
+  digitalWrite(Room1, HIGH);
+  digitalWrite(Room2, HIGH);
+  digitalWrite(Room3, HIGH);
+  digitalWrite(Room4, HIGH);
+  digitalWrite(Room5, HIGH);
+  digitalWrite(Room6, HIGH);
+  digitalWrite(Room7, HIGH);
+  digitalWrite(Room8, HIGH);
+  digitalWrite(Room9, HIGH);
+  digitalWrite(Room10, HIGH);
+  digitalWrite(Room11, HIGH);
+  digitalWrite(Room12, HIGH);
+  digitalWrite(Room13, HIGH);
+  digitalWrite(Room14, HIGH);
+  digitalWrite(Room15, HIGH);
+  digitalWrite(Room16, HIGH);
+
+
 }
--- PinDefinitions.h
+++ PinDefinitions.h
@@ -12,71 +12,96 @@ void PinInitialization(void);
 #define PumpWarmWaterMaxMassFlowRate 0.53 //[kg/s /power]
 
 // Pin mapping Arduino
-#define PinPumpCirculation    14 //
-#define PinValveWarmWaterOpen 15 //
-#define PinValveBoilerOpen     17 //
-#define PinValveSolarOpen     24 //
-#define PinValveHeatSource1Open 22 //
-#define PinStartHeatSource1   6
-#define PinValveWarmWaterClose 16 //
-#define PinValveBoilerClose    18 //
-#define PinValveSolarClose    25 //
-#define PinValveHeatSource1Close 23 //
-#define PinPumpWarmWater      14 //
-#define PinPumpBoiler         42 //
-#define PinPumpSolar          7 //
-#define PinPumpHeating        3 //
-#define PinMixerOpen          4
-#define PinMixerClose         5
-#define PinPulseCounter       19
+#define PinPumpCirculation        9
+#define PinValveWarmWaterOpen    39
+#define PinValveBoilerOpen       41
+#define PinValveSolarOpen        43
+#define PinValveHeatSource1Open  45
+#define PinStartHeatSource1      13
+#define PinValveWarmWaterClose   38
+#define PinValveBoilerClose      40
+#define PinValveSolarClose       42
+#define PinValveHeatSource1Close 44
+#define PinPumpWarmWater         10
+#define PinPumpBoiler            11
+#define PinPumpSolar             12
+#define PinPumpHeating            8
+#define PinMixerOpen              6
+#define PinMixerClose             7
+#define PinPulseCounter          19
+#define PinHeatControl            3
+// Valve Motor Sense Pins
+#define PinValveWarmWaterMotorSense   65 // A11 = 65
+#define PinValveBoilerMotorSense      64 // A10 = 64
+#define PinValveSolarMotorSense       63 // A9 = 63
+#define PinValveHeatSource1MotorSense 62 // A8 = 62
+// Room Heating
+#define Room1                    23
+#define Room2                    24
+#define Room3                    22
+#define Room4                    25
+#define Room5                    15
+#define Room6                    26
+#define Room7                    14
+#define Room8                    27
+#define Room9                     5
+#define Room10                   28
+#define Room11                   34
+#define Room12                   29
+#define Room13                   33
+#define Room14                   30
+#define Room15                   32
+#define Room16                   31
+
 // System Temperatures on Multiplexer 3
-#define SystempMultiplexer             3
-#define MultiplexTempHeatingLead       0
-#define MultiplexTempHeatingReturn     1
-#define MultiplexTempHeatSource1Lead   2 
-#define MultiplexTempHeatSource1Return 3
+#define SystempMultiplexer                3
+#define MultiplexTempHeatingLead          0
+#define MultiplexTempHeatingReturn        1
+#define MultiplexTempHeatSource1Lead      2 
+#define MultiplexTempHeatSource1Return    3
 #define MultiplexTempHeatSource1Operation 4
-#define MultiplexTempSolarReturn       5
-#define MultiplexTempSolarLead         6
-#define MultiplexTempBoilerCharge      7
-#define MultiplexTempBoilerReserve1    8
-#define MultiplexTempBoilerReserve2    9
-#define MultiplexTempBoilerHead       10
-#define MultiplexTempBoilerTop        11
-#define MultiplexTempWarmWater        12
-#define MultiplexTempCirculationReturn 13
-#define MultiplexIntensitySolar       14
-#define MultiplexTempOutside          15
+#define MultiplexTempSolarReturn          5
+#define MultiplexTempSolarLead            6
+#define MultiplexTempBoilerCharge         7
+#define MultiplexTempBoilerReserve1       8
+#define MultiplexTempBoilerReserve2       9
+#define MultiplexTempBoilerHead          10
+#define MultiplexTempBoilerTop           11
+#define MultiplexTempWarmWater           12
+#define MultiplexTempCirculationReturn   13
+#define MultiplexIntensitySolar          14
+#define MultiplexTempOutside             15
 // System Temperatures 
-#define OffsetTempHeatingLead       0
-#define OffsetTempHeatingReturn     0
-#define OffsetTempHeatSource1Lead   0 
-#define OffsetTempHeatSource1Return 0
-#define OffsetTempHeatSource1Operation 0
-#define OffsetTempSolarReturn       0
-#define OffsetTempSolarLead         0
-#define OffsetTempBoilerCharge      0
-#define OffsetTempBoilerReserve1    0
-#define OffsetTempBoilerReserve2    0
-#define OffsetTempBoilerHead       0
-#define OffsetTempBoilerTop        1
-#define OffsetTempWarmWater        2.35
-#define OffsetTempCirculationReturn 0
-#define OffsetIntensitySolar       0
-#define OffsetTempOutside          0 
+#define OffsetTempHeatingLead             0
+#define OffsetTempHeatingReturn           0
+#define OffsetTempHeatSource1Lead         0 
+#define OffsetTempHeatSource1Return       0
+#define OffsetTempHeatSource1Operation    0
+#define OffsetTempSolarReturn             0
+#define OffsetTempSolarLead               0
+#define OffsetTempBoilerCharge            0
+#define OffsetTempBoilerReserve1          0
+#define OffsetTempBoilerReserve2          0
+#define OffsetTempBoilerHead              0
+#define OffsetTempBoilerTop               1
+#define OffsetTempWarmWater               2.35
+#define OffsetTempCirculationReturn       0
+#define OffsetIntensitySolar              0
+#define OffsetTempOutside                 0 
+#define OffsetTempHeatControl             0
 
 // Multiplexer Control Pins
-#define MultiplexControl1 46
+#define MultiplexControl1 48
 #define MultiplexControl2 47
-#define MultiplexControl3 48
+#define MultiplexControl3 46
 #define MultiplexControl4 49
 // Multiplexer Temperature Input Pins
-#define MultiplexInput1 66
-// A12 = 66
-#define MultiplexInput2 67
-// A13 = 67
-#define MultiplexInput3 68
-// A14 = 68
+#define MultiplexInput1 66 // A12 = 66
+#define MultiplexInput2 67 // A13 = 67
+#define MultiplexInput3 68 // A14 = 68
+
+// PT1000 Sensor Pin
+#define TempHeatControl 69 // A15 = 69
 
 // Filter Coefficient
 #define AlphaFilter 0.7
