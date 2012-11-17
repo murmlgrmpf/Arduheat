788ef82e57427f79eb4826521eca1a8a32510987
Thomas Emmert <t.3mmert@gmail.com>
2012-11-17 17:29:31 +0100

    implemented new Valves using sense pin

--- cValve.cpp
+++ cValve.cpp
@@ -5,32 +5,48 @@ cValve::cValve(void)
 {
   _iPinOpen=0;
   _iPinClose=0;
-  _bState = false;
+  _iPinSense=0;
+  
+  _iTimePeriod = 1800;
+  
+  _bsetOpen = false;
+  _bisOpen = false;
+  _bisDriving = false;
 }
 
 /// Creates a valve object with pin setting, the initial state is close.
 /** Creates a valve object with pin setting. The Valve is initialized with a closed state. */
-cValve::cValve(int iPinOpen, int iPinClose)
+cValve::cValve(int iPinOpen, int iPinClose, int iPinSense)
 {
   cValve::setPinOpen(iPinOpen);
   cValve::setPinClose(iPinClose);
+  cValve::setPinSense(iPinSense);
   
-  _bState = false;
+  _iTimePeriod = 1800;
+  
+  _bsetOpen = false;
+  _bisOpen = false;
+  _bisDriving = true;
 }
 
 /// Creates a valve object with pin setting and an initial state.
-cValve::cValve(int iPinOpen, int iPinClose, boolean bState)
+cValve::cValve(int iPinOpen, int iPinClose, int iPinSense, boolean bsetOpen)
 {
   cValve::setPinOpen(iPinOpen);
   cValve::setPinClose(iPinClose);
+  cValve::setPinSense(iPinSense);
   
-  _bState = bState;
+  _iTimePeriod = 1800;
+  
+  _bsetOpen = bsetOpen;
+  _bisOpen = false;
+  _bisDriving = true;
 }
 
 /// Sets the state of the Valve and executes the run function of the valve.
-void cValve::set(boolean bState)
+void cValve::set(boolean bsetOpen)
 {
-  _bState = bState;
+  _bsetOpen = bsetOpen;
   
   cValve::run();
 }
@@ -40,8 +56,6 @@ void cValve::setPinOpen(int iPinOpen)
 {
   _iPinOpen = iPinOpen;
   pinMode(_iPinOpen, OUTPUT);
-  
-  cValve::run();
 }
 
 /// Sets the _PinClose. This is necessary for the rooms, where the constructor cannot initialize the pins.
@@ -49,27 +63,67 @@ void cValve::setPinClose(int iPinClose)
 {
   _iPinClose = iPinClose;
   pinMode(_iPinClose, OUTPUT);
-  
-  cValve::run();
 }
 
-/// Reads the state of the valve.
+/// Sets the _iPinSense. This is necessary for the rooms, where the constructor cannot initialize the pins.
+void cValve::setPinSense(int iPinSense)
+{
+  _iPinSense = iPinSense;
+  pinMode(_iPinSense, INPUT);
+}
+
+/// Reads the is state of the valve.
 /** \return True = open, False = closed. */
 boolean cValve::get(void)
 {
-  return _bState;
+  return _bisOpen;
 }
 
 /// Executes the state of the valve (opens or closes it).
-/** If the state of the valve is true, it shall be opened, if it is false, it is going to be closed*/
+/** If the set state of the valve is true, it shall be opened, if it is false, it is going to be closed*/
 void cValve::run(void)
 {
-  if(_bState==true)
+  unsigned long time = millis();
+  float fSense = analogRead(_iPinSense);
+  
+  // Reset timer and start driving if state of valve has changed
+  if(_bsetOpen!=_bisOpen)
   {
-    digitalWrite(_iPinOpen, LOW); // Valve gets openedPinValveWarmWaterOpen
+    _iTimeStart = time;
+    _bisDriving = true;
   }
-  else
+
+  // Drive the valve
+  if(_bisDriving==true)
   {
-    digitalWrite(_iPinOpen, HIGH); // Valve gets closed
+    // Check driving conditions
+    if((time - _iTimeStart < _iTimePeriod) && (fSense < SenseThreshold))
+    {
+      if(_bsetOpen == true)
+      {
+        // drive valve motor to open direction
+        digitalWrite(_iPinOpen, LOW);
+        digitalWrite(_iPinClose, HIGH);
+      } 
+      else
+      {
+        // drive valve motor to close direction
+        digitalWrite(_iPinOpen, HIGH);
+        digitalWrite(_iPinClose, LOW);
+      }
+      
+      // Set state of valve
+      _bisOpen = _bsetOpen;
+    }
+    else  // stop motor
+    {
+      // stop valve motor
+      digitalWrite(_iPinOpen, LOW);
+      digitalWrite(_iPinClose, LOW);
+      
+      // Set state of valve to not driving
+      _bisOpen = _bsetOpen;
+      _bisDriving = false;
+    }
   }
 }
--- cValve.h
+++ cValve.h
@@ -3,25 +3,32 @@
 
 #include "Arduino.h"
 
+#define SenseThreshold 180 // 0.6V threshold 5V max, 1023 resolution
 
 class cValve
 {
   private:
     int _iPinOpen;
     int _iPinClose;
-    boolean _bState;
+    int _iPinSense;
+    boolean _bsetOpen;
+    boolean _bisOpen;
+    boolean _bisDriving;
+    unsigned long _iTimeStart;
+    unsigned long _iTimePeriod;
     
     void run(void);
     
   public:
     cValve(void);
-    cValve(int iPinOpen,int iPinClose);
-    cValve(int iPinOpen,int iPinClose, boolean bState);
-    //~Pump();
+    cValve(int iPinOpen,int iPinClose, int iPinSense);
+    cValve(int iPinOpen,int iPinClose, int iPinSense, boolean bsetOpen);
+
     void setPinOpen(int iPinOpen);
     void setPinClose(int iPinClose);
+    void setPinSense(int iPinSense);
     boolean get();
-    void set(boolean bState);
+    void set(boolean bsetOpen);
 };
 
 #endif
