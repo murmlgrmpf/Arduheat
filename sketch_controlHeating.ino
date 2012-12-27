bb288c69b32615c4e85037a79e596cafab9db3f4
Thomas Emmert <t.3mmert@gmail.com>
2012-12-27 13:11:03 +0100

    implemented test sketch for temperatures

--- sketch_controlHeating.ino
+++ sketch_controlHeating.ino
@@ -1,6 +1,6 @@
 #include "Arduino.h"
 #include "PinDefinitions.h"
-//#include "cTemp.h"
+#include "cTemp.h"
 #include "cPump.h"
 #include "cRoom.h"
 #include "cHeating.h"
@@ -29,7 +29,8 @@ boolean trigger = false;
 
 float PumpState =0;
 
-cHeating Heating;
+// cHeating Heating;
+cTemp Temperatures;
 
 double Input, Output;
 
@@ -45,7 +46,7 @@ PID myPID(&Input, &Output, &SetpointTempWarmWater, 0.01, 0.001, 0.03, DIRECT);
 void  setup()
 {
   Serial.begin(57600);
-  //Serial.println("Steuerung");
+  Serial.println("Steuerung");
   attachInterrupt(4,incCounter,RISING);
   PinInitialization();
   delay(2000);// Give reader a chance to see the output.
@@ -53,7 +54,7 @@ void  setup()
   
   SerialDebug();
   DataAcquisition(true);
-  Heating.SetpointTempWarmWater = SetpointTempWarmWater;
+//   Heating.SetpointTempWarmWater = SetpointTempWarmWater;
   myPID.SetOutputLimits(0.0, 1.0);
   Output = 0.0;
 
@@ -108,63 +109,63 @@ void loop()
   //fMassFlowPumpWarmWater = Heating.HxWarmWater.calcMassflow(50.0, 8.0, Heating.TempBoilerTop(), Heating.FlowMeter.get() );
   //fMassFlowPumpWarmWater = Heating.HxWarmWater.calcMassflow(50.0, 8.0, 57.0, Heating.FlowMeter.get() );
   
-  if(Heating.FlowMeter.get()>0.0)
-  {
-   
-    Input = Heating.TempWarmWater();
-    
-   // timeoptimal control until 5 degrees below Setpoint
-   if(Heating.TempWarmWater()<=Heating.SetpointTempWarmWater - 7)
-   {
-     //turn the PID on
-     myPID.SetMode(MANUAL);
-     // open Valve
-     Heating.ValveWarmWater.set(true);
-     // Set Pump to maximum mass flow rate
-     Heating.PumpWarmWater.setMassFlowRate(Heating.PumpWarmWater.getMaxMassFlowRate());
-     Output = 0;
-   }
-   else if (Heating.TempWarmWater()>=Heating.SetpointTempWarmWater+2)
-   {
-    myPID.SetMode(MANUAL);
-    // stop pump
-    Heating.PumpWarmWater.setMassFlowRate(0.0);
-    // close valve
-    Heating.ValveWarmWater.set(false);
-    
-    Output = 0;
-   }
-   else
-   {
-     // open Valve
-     Heating.ValveWarmWater.set(true);
-     
-     // open loop control
-     fMassFlowPumpWarmWater = Heating.HxWarmWater.calcMassflow(Heating.SetpointTempWarmWater, 8.1560, Heating.TempBoilerTop(), Heating.FlowMeter.get() );
+//   if(Heating.FlowMeter.get()>0.0)
+//   {
+//    
+//     Input = Heating.TempWarmWater();
+//     
+//    timeoptimal control until 5 degrees below Setpoint
+//    if(Heating.TempWarmWater()<=Heating.SetpointTempWarmWater - 7)
+//    {
+//      turn the PID on
+//      myPID.SetMode(MANUAL);
+//      open Valve
+//      Heating.ValveWarmWater.set(true);
+//      Set Pump to maximum mass flow rate
+//      Heating.PumpWarmWater.setMassFlowRate(Heating.PumpWarmWater.getMaxMassFlowRate());
+//      Output = 0;
+//    }
+//    else if (Heating.TempWarmWater()>=Heating.SetpointTempWarmWater+2)
+//    {
+//     myPID.SetMode(MANUAL);
+//     stop pump
+//     Heating.PumpWarmWater.setMassFlowRate(0.0);
+//     close valve
+//     Heating.ValveWarmWater.set(false);
+//     
+//     Output = 0;
+//    }
+//    else
+//    {
+//      open Valve
+//      Heating.ValveWarmWater.set(true);
+//      
+//      open loop control
+//      fMassFlowPumpWarmWater = Heating.HxWarmWater.calcMassflow(Heating.SetpointTempWarmWater, 8.1560, Heating.TempBoilerTop(), Heating.FlowMeter.get() );
 //      Heating.PumpWarmWater.setMassFlowRate(fMassFlowPumpWarmWater);
-     
-     // closed loop control
-     //turn the PID on
-     myPID.SetMode(AUTOMATIC);
-     myPID.Compute();
+//      
+//      closed loop control
+//      turn the PID on
+//      myPID.SetMode(AUTOMATIC);
+//      myPID.Compute();
 //      float theta = (Heating.TempBoilerTop()-Heating.SetpointTempWarmWater)/(Heating.TempBoilerTop()-8.1560);
 //      float beta = 1;// - 1.5*theta;
-      Heating.PumpWarmWater.setMassFlowRate(Output*Heating.PumpWarmWater.getMaxMassFlowRate());
-     
+//       Heating.PumpWarmWater.setMassFlowRate(Output*Heating.PumpWarmWater.getMaxMassFlowRate());
+//      
 //      Heating.PumpWarmWater.setMassFlowRate(Heating.FlowMeter.get());
-     
-   }
-
-  }
-  else
-  {
-    // stop pump
-    Heating.PumpWarmWater.setMassFlowRate(0.0);
-    // close valve
-    Heating.ValveWarmWater.set(false);
-  }
-  //***************************
-  
+//      
+//    }
+// 
+//   }
+//   else
+//   {
+//     stop pump
+//     Heating.PumpWarmWater.setMassFlowRate(0.0);
+//     close valve
+//     Heating.ValveWarmWater.set(false);
+//   }
+//   ***************************
+//   
   
   //SerialDebug();
   DataAcquisition(false);
@@ -174,7 +175,7 @@ void loop()
 
 void incCounter()
 {
-  Heating.FlowMeter.incCounter();
+//   Heating.FlowMeter.incCounter();
 }
 
 
@@ -190,12 +191,14 @@ void DataAcquisition(boolean bfirstRun)
   if(trigger)
   {
     printValue(time);
-    printValue(Heating.PumpWarmWater._Power);
-    printValue(Heating.PumpWarmWater.getMassFlowRate());
-    printValue(Heating.FlowMeter.get());
-    printValue(Heating.TempWarmWater());
-    printValue(Heating.TempBoilerTop());
-    printValue(Heating.TempCirculationReturn());
+//     printValue(Heating.PumpWarmWater._Power);
+//     printValue(Heating.PumpWarmWater.getMassFlowRate());
+//     printValue(Heating.FlowMeter.get());
+//     printValue(Heating.TempWarmWater());
+//     printValue(Heating.TempBoilerTop());
+//     printValue(Heating.TempCirculationReturn());
+    printValue(readTemperature(3,15));
+    printValue(Temperatures.getTemp(3,15));
     printValue(Output);
     //printValue();
     
@@ -219,19 +222,19 @@ void SerialDebug()
 //***********    Write Counter of Warmwater flow rate ***********
     Serial.println("Testing Heat Exchanger function. Values should be equal to the ones in brackets");
     Serial.println("Soll Cr Pump WarmWater 100C (1.9786)");
-    Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 100.0, 1.0), DEC);
+//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 100.0, 1.0), DEC);
     Serial.println("Soll Cr Pump WarmWater 90C (1.9786)");
-    Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 90.0, 1.0), DEC);
+//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 90.0, 1.0), DEC);
     Serial.println("Soll Cr Pump WarmWater 80C (1.5109)");
-    Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 80.0, 1.0), DEC);
+//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 80.0, 1.0), DEC);
     Serial.println("Soll Cr Pump WarmWater 70C (1.2628)");
-    Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 70.0, 1.0), DEC);
+//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 70.0, 1.0), DEC);
     Serial.println("Soll Cr Pump WarmWater 60C (0.9790)");
-    Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 60.0, 1.0), DEC);
+//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 60.0, 1.0), DEC);
     Serial.println("Soll Cr Pump WarmWater 55C (0.6681)");
-    Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 55.0, 1.0), DEC);
+//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 55.0, 1.0), DEC);
     Serial.println("Soll Cr Pump WarmWater 52C (0.6681)");
-    Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 52.0, 1.0), DEC);
+//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 52.0, 1.0), DEC);
 //***********    Write Systemtemperatures for calibration ***********
   }
 }
