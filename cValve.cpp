782209339713bed139f02e0c8239f97da44702cf
murmlgrmpf <t.3mmert@googlemail.com>
2014-01-01 22:23:56 +0100

    simplified valve code

--- cValve.cpp
+++ cValve.cpp
@@ -1,21 +1,5 @@
 #include "cValve.h"
 
-/// Creates a valve object with pin setting, the period is set to a standard value and the initial state is close.
-cValve::cValve(int iPinOpen, int iPinClose)
-{
-  _iPinOpen=iPinOpen;
-  _iPinClose=iPinClose;
-  
-  _iTimeStart = millis();
-  _iTimePeriod = 1500;
-  
-  _bsetOpen = false;
-  _bisOpen = false;
-  _bisDriving = false;
-  
-  run();
-}
-
 /// Creates a valve object with pin setting and period, the initial state is close.
 /** Creates a valve object with pin setting. The Valve is initialized with a closed state. */
 cValve::cValve(int iPinOpen, int iPinClose, unsigned long iTimePeriod)
@@ -26,11 +10,8 @@ cValve::cValve(int iPinOpen, int iPinClose, unsigned long iTimePeriod)
   _iTimeStart = millis();
   _iTimePeriod = iTimePeriod;
   
-  _bsetOpen = false;
   _bisOpen = false;
-  _bisDriving = true;
-  
-  run();
+  set(false);
 }
 
 /// Sets the state of the Valve and executes the run function of the valve.
@@ -51,43 +32,27 @@ boolean cValve::get(void)
 /// Executes the set state of the valve (opens or closes it).
 /** If the set state of the valve is true, it shall be opened, if it is false, it is going to be closed*/
 void cValve::run(void)
-{  
-  // Reset timer and start driving if state of valve has changed
-  if(_bsetOpen!=_bisOpen)
-  {
-    _iTimeStart = millis();
-	_bisOpen = _bsetOpen;
-    _bisDriving = true;
-  }
+{
+	// Reset timer and start driving if state of valve has changed
+	if(_bsetOpen!=_bisOpen)
+	{
+		_iTimeStart = millis();
+		_bisOpen = _bsetOpen;
+	}
 
-  // Drive the valve
-  if(_bisDriving==true)
-  {
-    // Check if motor has reached end position
-    if(millis() < _iTimeStart + _iTimePeriod)
-    {
-      if(_bsetOpen == true)
-      {
-        // drive valve motor to open direction
-        digitalWrite(_iPinOpen, LOW);
-        digitalWrite(_iPinClose, HIGH);
-      } 
-      else
-      {
-        // drive valve motor to close direction
-        digitalWrite(_iPinOpen, HIGH);
-        digitalWrite(_iPinClose, LOW);
-      }
-      
-    }
-    else  // stop motor
-    {
-      // stop valve motor
-      digitalWrite(_iPinOpen, LOW);
-      digitalWrite(_iPinClose, LOW);
-      
-      // Set state of valve to not driving
-      _bisDriving = false;
-    }
-  }
+	// Drive the valve
+	if(millis() < _iTimeStart + _iTimePeriod)
+	{
+		// drive valve motor to open direction if _bsetOpen is true/high
+		// otherwise drive to close
+		digitalWrite(_iPinOpen, !_bsetOpen);
+		digitalWrite(_iPinClose, _bsetOpen);
+
+	}
+	else  // stop motor
+	{
+		// stop valve motor
+		digitalWrite(_iPinOpen, LOW);
+		digitalWrite(_iPinClose, LOW);
+	}
 }
--- cValve.h
+++ cValve.h
@@ -10,15 +10,13 @@ class cValve
     int _iPinClose;
     boolean _bsetOpen;
     boolean _bisOpen;
-    boolean _bisDriving;
     unsigned long _iTimeStart;
     unsigned long _iTimePeriod;
     
     void run(void);
     
   public:
-    cValve(int iPinOpen, int iPinClose);
-    cValve(int iPinOpen,int iPinClose, unsigned long iTimePeriod);
+    cValve(int iPinOpen,int iPinClose, unsigned long iTimePeriod = 1500);
 
     boolean get();
     void set(boolean bsetOpen);
