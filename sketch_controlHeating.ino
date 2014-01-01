c497a598c31f9e9d08a3af1a04e0e017d1696ffd
murmlgrmpf <t.3mmert@googlemail.com>
2014-01-01 20:06:34 +0100

    update arduino sketch

--- sketch_controlHeating.ino
+++ sketch_controlHeating.ino
@@ -43,12 +43,12 @@ void  setup()
   delay(2000);// Give reader a chance to see the output.
   analogReference(DEFAULT);
   
-  SerialDebug();
   DataAcquisition(true);
   // Overwrite Setpoint Temperature for WarmWater
   //Heating.SpTempWarmWater = 45.0;
 
 }
+
 
 
 
@@ -58,46 +58,8 @@ void  setup()
  */
 void loop()
 {
-  // Wärmequellen management
-  // #1 Brenner
-  //Heating.ControlBurner();
-//  float fmaxHeatflow =0;
-  
-  
-//  for(int i=0; i<16;i++)
-//  {
-//    Heating.Rooms[i].setTempRefSchedule(fTempRefSchedule[i]);
-//    // Open valves of rooms that need heat
-//    if (Heating.Rooms[i].getHeatflow()>0)
-//    {
-////      digitalWrite(Heating.Rooms[i].getPinValve(), LOW);
-//    }
-//    else // Close valves for rooms that do not need heat
-//    {
-////      digitalWrite(Rooms[i].getPinValve(), HIGH);
-//    }
-//    
-//    // Identify maximum required Heatflow
-//    if (Heating.Rooms[i].getHeatflow()<fmaxHeatflow)
-//    {
-//      fmaxHeatflow = Heating.Rooms[i].getHeatflow();
-//    }
-//    
-////    PumpState = Heating.Rooms[i].getTempActual();
-////    PumpState = Heating.Rooms[i].getTempRef();
-////    PumpState = Heating.Rooms[i].getHeatflow();
-//    
-//  }
-  //Heating.Rooms[1].getHeatflow();
-  //Heating.ValveWarmWater.set(false);
-  //Heating.ValveWarmWater.set(true);
-
-  //Heating.PumpSolar.set(0.5);
-  //Heating.PumpHeating.set(0.5);
-  //Heating.PumpBoiler.set(0.50);
-  //Heating.PumpWarmWater.set(0.5);
-  
-  Heating.ControlWarmWater();
+   Heating.Control();
+   Heating.ControlWarmWater();
   
   DataAcquisition(false);
   CheckTime();
@@ -106,7 +68,7 @@ void loop()
 
 void incCounter()
 {
-  Heating.FlowMeter.incCounter();
+   Heating.FlowMeter.incCounter();
 }
 
 
@@ -115,18 +77,28 @@ void DataAcquisition(boolean bfirstRun)
 {
   // Header
   if (bfirstRun) {
-    Serial.println("Time ; TempWarmWater ; TempBoilerTop ; TempBoilerReturn ; TempBoilerHead ; PID% ;");
+    Serial.println("Time; Endtime; _bFLame; TempBoilerTop; SpTempChargeWarmWater; TempBoilerHead; TempBoilerCharge; PID%; haveWarmWater; needWarmWater; SpTempChargeHeating");
+        printValue(Heating.Burner._bFlame);
   }
-  
+  unsigned long endtime = 0;
   unsigned long time = millis();
   if(trigger)
   {
     printValue(time);
-    printValue(Heating.TempWarmWater());
-    printValue(Heating.Boiler.TempBoilerTop());
-    printValue(Heating.TempCirculationReturn());
-    printValue(Heating.Boiler.TempBoilerHead());
-    printValue(Heating.PumpWarmWater.Power);
+    endtime = Heating.Burner._StartTime+Heating.Burner._MinBurnTime;
+    printValue(endtime);
+    printValue(Heating.Burner._bFlame);
+    printValue(Heating.Boiler.TempBoilerTop.get());
+    printValue(Heating.Boiler.SpTempChargeWarmWater());
+    printValue(Heating.Boiler.TempBoilerHead.get());
+    printValue(Heating.Boiler.TempBoilerCharge.get());
+    printValue(Heating.Boiler.PumpBoilerCharge.Power);
+    printValue(Heating.Boiler.haveWarmWater());
+    printValue(Heating.Boiler.needWarmWater());
+    printValue(Heating.Boiler.needHeating());
+    printValue(Heating.Boiler.SpTempChargeHeating());
+
+    
     //printValue();
     
     Serial.println(); //carriage return and new line
@@ -137,33 +109,6 @@ void printValue(float fValue)
 {
   Serial.print(fValue,DEC);
   Serial.print(" ; "); // separator
-}
-
-void SerialDebug()
-{
-  unsigned long time = millis();
-//   if(trigger)
-  {
-//  Serial.println("************");
-//  Serial.println(PumpState, DEC);
-//***********    Write Counter of Warmwater flow rate ***********
-    Serial.println("Testing Heat Exchanger function. Values should be equal to the ones in brackets");
-    Serial.println("Soll Cr Pump WarmWater 100C (1.9786)");
-//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 100.0, 1.0), DEC);
-    Serial.println("Soll Cr Pump WarmWater 90C (1.9786)");
-//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 90.0, 1.0), DEC);
-    Serial.println("Soll Cr Pump WarmWater 80C (1.5109)");
-//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 80.0, 1.0), DEC);
-    Serial.println("Soll Cr Pump WarmWater 70C (1.2628)");
-//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 70.0, 1.0), DEC);
-    Serial.println("Soll Cr Pump WarmWater 60C (0.9790)");
-//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 60.0, 1.0), DEC);
-    Serial.println("Soll Cr Pump WarmWater 55C (0.6681)");
-//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 55.0, 1.0), DEC);
-    Serial.println("Soll Cr Pump WarmWater 52C (0.6681)");
-//     Serial.println(Heating.HxWarmWater.calcMassflow(50.0, 8.0, 52.0, 1.0), DEC);
-//***********    Write Systemtemperatures for calibration ***********
-  }
 }
 
 void CheckTime()
