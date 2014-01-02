ed2587de1177ed72bf29674146df5ff2cfa2a385
murmlgrmpf <t.3mmert@googlemail.com>
2014-01-02 21:20:14 +0100

    removed massflow calculations, directly set power added lfpwm

--- cPump.cpp
+++ cPump.cpp
@@ -1,109 +1,35 @@
 #include "cPump.h"
 
-//#define PinPump 51
 
-cPump::cPump(void)
+cPump::cPump(int PinPump, float fPower):
+PWM(4000)
 {
-  cPump::cPump(0, 1.0, 0.0);
+	_PinPump = PinPump;
+	Power = fPower;
+	setPower(Power);
 }
 
-cPump::cPump(int PinPump, float fMaxMassFlowRate)
+
+void cPump::setPower( float fPower )
 {
-  cPump::cPump(PinPump, fMaxMassFlowRate, 0.0);
+	_Power = fPower;
+	
+	// Limit _Power
+	if (_Power>1.0)
+	{
+		_Power = 1.0;
+	}
+	else if(_Power<0.0)
+	{
+		_Power = 0.0;
+	}
+	
+	// Pump is running if switching time of PWM is not yet exceeded
+	digitalWrite(_PinPump, !PWM.get(_Power)); // Pump switched on if true and off if false
 }
 
-cPump::cPump(int PinPump, float fMaxMassFlowRate, float fMassFlowRate)
+
+float cPump::getPower(void)
 {
-  _PinPump = PinPump;
-  _fMaxMassFlowRate = fMaxMassFlowRate;
-  //Set the time to switch on and off the Pump
-  _LastTimePeriod = millis();
-  _Period = 4000;
-  Power = 0;
-
-  cPump::setMassFlowRate(fMassFlowRate);
-}
-
-/* Switches the Pump on and off corresponding to the power level
-   After each Period the length of the period gets adjusted.
-*/
-void cPump::run(void)
-{
-  // Calculate the Period
-  cPump::calcPeriod();
-  // Set the end time of the current Period
-  _TimePeriod = round(_LastTimePeriod + _Period);
-  // Set the next switching Time
-  _TimeSwitch = round(_LastTimePeriod + _Power*_Period);
-  // TimeSwitch reached
-  if(millis()>=_TimeSwitch)
-  {
-    digitalWrite(_PinPump, HIGH); // Pump switched off
-  }
-  // The Switching time got readjusted and the Pump 
-  // needs to be switched on again
-  else
-  {
-    digitalWrite(_PinPump, LOW); // Pump switched on
-  }
-  // TimePeriod is over and new period starts
-  if(millis()>=_TimePeriod)
-  {
-    //Set the next Last Period Time
-    _LastTimePeriod = millis();
-  }
-}
-
-float cPump::getMassFlowRate()
-{
-  float fMassFlowRate = _Power*_fMaxMassFlowRate;
-  return(fMassFlowRate);
-}
-
-/* Calculates the pwm _Power and then executes the pwm
-*/
-void cPump::setMassFlowRate(float fMassFlowRate)
-{
-  //Linear approximation of mass flow rate over high percentage of pwm.
-  _Power = fMassFlowRate/_fMaxMassFlowRate;
-  
-  // Limit _Power 
-  if (_Power>1.0)
-  {
-    _Power = 1.0;
-  }
-  else if(_Power<0.0)
-  {
-    _Power = 0.0;
-  }
-  
-  // Execute pwm 
-  cPump::run();
-
-}
-
-// May be needed to limit the regulator
-float cPump::getMaxMassFlowRate()
-{
-  return _fMaxMassFlowRate;
-}
-
-// Needed for calibration of pwm
-void cPump::setMaxMassFlowRate(float fMaxMassFlowRate)
-{
-  _fMaxMassFlowRate = fMaxMassFlowRate;
-}
-
-void cPump::setPinPump(int PinPump)
-{
-  _LastTimePeriod = millis();
-  _PinPump = PinPump;
-  cPump::run();
-
-}
-
-void cPump::calcPeriod()
-{
-  //Default _Period = 10 sec = 10000ms
-  _Period = 2000;
+	return _Power;
 }
--- cPump.h
+++ cPump.h
@@ -3,59 +3,32 @@
 
 #include "Arduino.h"
 #include "PinDefinitions.h"
+#include "cLFPWM.h"
 
 class cPump
 {
-  private:
-    int _PinPump;
-    
-    float _Period;
-    unsigned long _TimePeriod;
-    unsigned long _TimeSwitch;
-    unsigned long _LastTimePeriod;
-    float _fMaxMassFlowRate;
-    
-    /// Execute the pump. 
-    /** Each time, it is checked whether the period is over or whether the switch time is reached. 
-        If the TimeSwitch is reached, the pump is switched off. When the TimePeriod is reached, the pump is switched on 
-        and the new TimePeriod, TimeSwitch and LastTimePeriod get set. */ 
-    void run(void);
-    /// Calculate the Period
-    /** A more elaborate way to calculate the Period of the "slow" PWM could be implemented in order
-    to prevent the pump from being switched on and off for only short times in some race conditions.*/
-    void calcPeriod(void);
-    
-  public:
-  
-  float _Power;
-  double Power;
-    /// Creates a pump object without a certain pin.
-    cPump(void);
-    /// Creates a pump object with a certain pin.
-    /** \param PinPump the pin used to drive the Pump*/
-    /** \param fMaxMassFlowRate the maximum mass flow rate of the pump*/
-    cPump(int PinPump, float fMaxMassFlowRate);
-    /// Creates a pump object with a certain pin and a certain value
-    /** \param PinPump the pin used to drive the Pump
-        \param Value the power in percent    */
-    cPump(int PinPump, float fMaxMassFlowRate, float fMassFlowRate);
-    /// Get the actual mass flow rate in kg/s
-    /** \return the actual mass flow rate in kg/s */
-    float getMassFlowRate(void);
-    /// Set the mass flow rate in kg/s
-    /** Set the mass flow rate in kg/s and execute the run function
-    \param Value the desired power in percent */
-    void setMassFlowRate(float fMassFlowRate);
-    /// Get the maximum mass flow rate in kg/s
-    /** \return the maximum mass flow rate in kg/s for limiting the controler/regulator*/
-    float getMaxMassFlowRate(void);
-    /// Set the maximum mass flow rate in kg/s
-    /** \return the maximum mass flow rate in kg/s for limiting the controler/regulator*/
-    void setMaxMassFlowRate(float fMaxMassFlowRate);
-    /// Set the Pin of the Pump
-    /** Set the pin of the pump and execute the run function
-    \param PinPump the pin of the pump */
-    void setPinPump(int PinPump);
+	private:
+	int _PinPump;
+	float _Power;
+	
+	cLFPWM PWM;
+	
+	public:
+	double Power;
+	
+	/// Creates a pump object with a certain pin.
+	/** The power of the pump is set to zero by default.*/
+	/** \param PinPump the pin used to drive the Pump*/
+	/** \param Power the power the pump in percent*/
+	cPump(int PinPump, float fPower = 0.0);
+	/// Set the power of the pump in percent
+	/** Set the power of the pump in percent and execute
+	\param Value of the desired power in percent*/
+	void setPower(float fPower);
+	/// Get the power of the pump in percent
+	/** \return power of the pump in percent */
+	float getPower(void);
+
 };
 
 #endif
