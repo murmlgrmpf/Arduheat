fadb985a2d90d742b8ff7aeed93c757eadfe6818
Thomas Emmert <t.3mmert@gmail.com>
2015-04-04 13:18:12 +0200

    reenabled webserver

--- Arduheat.ino
+++ Arduheat.ino
@@ -1,24 +1,23 @@
 #include <Arduino.h>
-// #include <avr/pgmspace.h>
 #include "cWarmWater.h"// important for global TimeNow variable
 #include "cHeating.h"
 #include <cPID.h>
 
-/*
+
 #include "config.h"
 #include <SdFat.h>
 #include <SdFatUtil.h>
-*/
+
 #include <ArduinoJson.h>
 #include <avr/pgmspace.h>
 
-/*
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <WebServer.h>
 
 #include "fileServer.h"
-*/
+
 // Speicherbedarf: 
 // 2168 ohne logging und webserver
 // 4352 mit  logging und webserver
@@ -26,22 +25,23 @@
 /* CHANGE THIS TO YOUR OWN UNIQUE VALUE.  The MAC number should be
  * different from any other devices on your network or you'll have
  * problems receiving packets. */
-//static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 
 /* CHANGE THIS TO MATCH YOUR HOST NETWORK.  Most home networks are in
  * the 192.168.0.XXX or 192.168.1.XXX subrange.  Pick an address
  * that's not in use and isn't going to be automatically allocated by
  * DHCP from your router. */
-//static uint8_t ip[] = { 192, 168, 123, 250 };
+static uint8_t ip[] = { 192, 168, 123, 250 }; // Home Emmert
+//static uint8_t ip[] = { 192, 168, 179, 250 }; // Home Thomas
 
 
 /* This creates an instance of the webserver.  By specifying a prefix
  * of "", all pages will be at the root of the server. */
-//#define PREFIX ""
-//WebServer webserver(PREFIX, 80);
+#define PREFIX ""
+WebServer webserver(PREFIX, 80);
 
-#include <Mailbox.h>
+// #include <Mailbox.h>
 
 
 #include "cTrigger.h"
@@ -70,8 +70,8 @@ void  setup()
 	pinMode(53, OUTPUT);                       // set the SS pin as an output (necessary!)
 	digitalWrite(53, HIGH);                    // but turn off the W5100 chip!
 	
-	Bridge.begin();
-	Mailbox.begin();
+// 	Bridge.begin();
+// 	Mailbox.begin();
 	
 	//// RTC test //////////
 	Serial.begin(115200);
@@ -90,7 +90,7 @@ void  setup()
 	analogReference(DEFAULT);
 	//analogReference(EXTERNAL);
 	
-	/*
+	
 	initSD();
 	writeConf(&Heating); // write defaults
 	readConf( &Heating);
@@ -99,25 +99,25 @@ void  setup()
 	startLogging(&Heating);
 	
 	/* initialize the Ethernet adapter */
-	//Ethernet.begin(mac, ip);
+	Ethernet.begin(mac, ip);
 	
-	//initFileServer();
+	initFileServer();
 
 	/* setup our default command that will be run when the user accesses
 	* the root page on the server */
-	//webserver.setDefaultCommand(&helloCmd);
+	webserver.setDefaultCommand(&helloCmd);
 
 	/* run the same command if you try to load /index.html, a common
 	* default page name */
-	//webserver.addCommand("index.html", &helloCmd);
+	webserver.addCommand("index.html", &helloCmd);
 	
-	//webserver.addCommand("rest.html", &restCmd);
+	webserver.addCommand("rest.html", &restCmd);
 	
 	// Fileserver
-	//webserver.setUrlPathCommand(&fsAccessCmd);
+	webserver.setUrlPathCommand(&fsAccessCmd);
 
 	/* start the webserver */
-	//webserver.begin();
+	webserver.begin();
 }
 
 /**
@@ -129,7 +129,7 @@ void loop()
 	Heating.Control();
 	Heating.WarmWater.Control();
 	
-	/*
+	
 	if (trigger.get()) {
 		// Generate new logfile every day at midnight
 		if (TimeNow.hour()-rtc.now().hour()==23){
@@ -151,7 +151,7 @@ void loop()
 	
 	char buff[64];
 	int len = 64;
-	*/
+	
 	/* process incoming connections one at a time forever */
-	//webserver.processConnection(buff, &len);
+	webserver.processConnection(buff, &len);
 }
