ec922edb6a880635b8691f2218ba6b18793befed
Thomas Emmert <t.3mmert@gmail.com>
2015-04-04 13:32:39 +0200

    eliminated schedule

--- cWarmWater.cpp
+++ cWarmWater.cpp
@@ -5,66 +5,33 @@ void cWarmWater::Control(void){
 	// with the power determined by pid controller.
 	// Else switch pid to manual and stop pump.
 	if(!digitalRead(PinWarmWaterSwitch))
-	Pump.run(SpTemp(), IsTemp.get());
+		Pump.run(SpTemp, IsTemp.get());
 	else
-	Pump.run();
+		Pump.run();
 }
-
 void cWarmWater::getSP( JsonObject& root )
 {
-	root["Wts0"] = TempSchedule[0].time.totalseconds();
-	root["WTs0"] = TempSchedule[0].temp;
-	root["Wts1"] = TempSchedule[1].time.totalseconds();
-	root["WTs1"] = TempSchedule[1].temp;
+	root["WWTs"] = SpTemp;
 }
+
 
 int cWarmWater::setSP( JsonObject& root )
 {
-	int fail = 0;
-	int posReturn =0;
+	if(root.containsKey("WWTs")) {
+		if(root["WWTs"].is<double>()) {
+			SpTemp = root["WWTs"].as<double>();
+			return true;
+		}
+	}
 	
-	if(root.containsKey("Wts0")) {
-		if(root["WTs0t"].is<long>()) {
-			TempSchedule[0].time.set(root["WTs0t"].as<long>());
-			posReturn++;
-		}
-		else fail=1;
-	}
-	else fail=1;
-	if(root.containsKey("WTs0")) {
-		if(root["WTs0T"].is<double>()) {
-			TempSchedule[0].temp = root["WTs0T"].as<double>();
-			posReturn++;
-		}
-		else fail=1;
-	}
-	else fail=1;
-	if(root.containsKey("Wts1")) {
-		if(root["Wts1"].is<long>()) {
-			TempSchedule[1].time.set(root["Wts1"].as<long>());
-			posReturn++;
-		}
-		else fail=1;
-	}
-	else fail=1;
-	if(root.containsKey("WTs1")) {
-		if(root["WTs1"].is<double>()) {
-			TempSchedule[1].temp = root["WTs1"].as<double>();
-			posReturn++;
-		}
-		else fail=1;
-	}
-	else fail=1;
-	
-	// If all three parameter objects were successfully filled
-	if (fail == 0) return posReturn;
-	else return 0;
+	return false;
 }
 
 void cWarmWater::getData( JsonObject& root )
 {
 	root["WP"] = Pump.get();
 	root["WTi"] =  IsTemp.get();
-	root["Wn"] = !digitalRead(PinWarmWaterSwitch);
+	
+	root["Wn"] = static_cast<int>(!digitalRead(PinWarmWaterSwitch));
 	root["WTitoB"] = IsTempWarmWaterToBoiler.get();
 }
--- cWarmWater.h
+++ cWarmWater.h
@@ -10,13 +10,9 @@
 #include "cPump.h"
 #include "PinDefinitions.h"
 #include <cPID.h>
-#include <RTClib.h>
 #include <ArduinoJson.h>
 
 #define DefaultSpTempWarmWater 45.0
-#define DefaultSpTempWarmWaterLower -2.0
-
-extern DateTime TimeNow;
 
 class cWarmWater
 {
@@ -27,38 +23,21 @@ class cWarmWater
 	IsTempWarmWaterToBoiler(&MPNumSys[0], &MPChanSys[idxTempWarmWaterToBoiler], &SysTempOffset[idxTempWarmWaterToBoiler])
 	{
 		pinMode(PinWarmWaterSwitch, INPUT_PULLUP);
-		// Initialize WarmWater Schedule
-		TempSchedule[0].time.set(0, 6,0,0);
-		TempSchedule[0].temp = DefaultSpTempWarmWater;
-		TempSchedule[1].time.set(0,20,0,0);
-		TempSchedule[1].temp = DefaultSpTempWarmWater + DefaultSpTempWarmWaterLower;
+		SpTemp = DefaultSpTempWarmWater;
 		// Initialize PID controllers for pump
 		Pump.SetOutputLimits(0.0, 1.0);
 	}
 	
 	void Control(void);
 	
-	double SpTemp(void)
-	{
-		double SpTemp = TempSchedule[1].temp;
-		TimeSpan rel;
-		rel.set(0, TimeNow.hour(), TimeNow.minute(), 0);
-		
-		for(int iSwitch=0; iSwitch<2; iSwitch++) {
-			if (TempSchedule[iSwitch].time.totalseconds() < rel.totalseconds())
-				SpTemp = TempSchedule[iSwitch].temp;
-		}
-		return SpTemp;
-	}
-	
 	void getSP(JsonObject& root);
 	int setSP(JsonObject& root);
 	
 	void getData(JsonObject& root);
+	double SpTemp;
 	
-	sTempSchedule TempSchedule[2];
-
-	//private:
+	private:
+	
 	cPump Pump;
 	cTempSensor IsTemp;
 	cTempSensor IsTempWarmWaterToBoiler;
