537077be605af437ceb9c176b7a2f36d86f879cf
Thomas Emmert <t.3mmert@gmail.com>
2015-08-02 17:25:17 +0200

    dynamic pid and setpoint for hot and cold Boiler

--- cWarmWater.cpp
+++ cWarmWater.cpp
@@ -8,12 +8,23 @@ void cWarmWater::Control(void){
 		Pump.run(SpTemp, IsTemp.get());
 	else
 		Pump.run();
+	
+	if(Trigger.get())
+		setParam();
 }
+
+void cWarmWater::setParam()
+{
+	double P = min(0.5,max(0.1, 1.127-0.011*IsTempBoilerTop.get()));
+	double D = min(7.0,max(2.6,17.9-0.19*IsTempBoilerTop.get()));
+	SpTemp = max(45, 0.143*IsTempBoilerTop.get() + 37.1);
+	Pump.SetTunings(P, Pump.GetKi(), D);
+}
+
 void cWarmWater::getSP( JsonObject& root )
 {
 	root["WWTs"] = SpTemp;
 }
-
 
 int cWarmWater::setSP( JsonObject& root )
 {
--- cWarmWater.h
+++ cWarmWater.h
@@ -12,35 +12,38 @@
 #include <cPID.h>
 #include <ArduinoJson.h>
 
-#define DefaultSpTempWarmWater 45.0
-
 class cWarmWater
 {
 	public:
 	cWarmWater(void):
-	Pump(PinPumpWarmWater,0.5, 0.0, 7.0, DIRECT),
+	Pump(PinPumpWarmWater,0.12, 0.005, 2.8, DIRECT),
+	Trigger(15*60*1000),
 	IsTemp(&MPNumSys[0], &MPChanSys[idxTempWarmWater], &SysTempOffset[idxTempWarmWater]),
+	IsTempBoilerTop(&MPNumSys[0], &MPChanSys[idxTempBoilerTop], &SysTempOffset[idxTempBoilerTop]),
 	IsTempWarmWaterToBoiler(&MPNumSys[0], &MPChanSys[idxTempWarmWaterToBoiler], &SysTempOffset[idxTempWarmWaterToBoiler])
 	{
 		pinMode(PinWarmWaterSwitch, INPUT_PULLUP);
-		SpTemp = DefaultSpTempWarmWater;
 		// Initialize PID controllers for pump
 		Pump.SetOutputLimits(0.0, 1.0);
 		Pump.SetSampleTime(500);
+		setParam();
 	}
 	
 	void Control(void);
 	
 	void getSP(JsonObject& root);
 	int setSP(JsonObject& root);
-	
 	void getData(JsonObject& root);
+	
 	double SpTemp;
 	
 	private:
+	void setParam();
 	
 	cPump Pump;
+	cTrigger Trigger;
 	cTempSensor IsTemp;
+	cTempSensor IsTempBoilerTop;
 	cTempSensor IsTempWarmWaterToBoiler;
 };
 
