467f2b68ed6b932d3f48055904b8b47eea97ac85
Thomas Emmert <t.3mmert@gmail.com>
2015-12-30 18:15:17 +0100

    implemented yun bridge replacement

---
+++ Python/bridgeSerial.py
@@ -0,0 +1,137 @@
+__author__ = 'Copyright (c) 2013 Alan Yorinks All rights reserved.'
+"""
+
+@author: Alan Yorinks
+Copyright (c) 2013-15 Alan Yorinks All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU  General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+import threading
+import time
+import sys
+
+import serial
+
+
+class bridgeSerial(threading.Thread):
+    """
+     This class manages the serial port for Arduino serial communications
+    """
+
+    # class variables
+    arduino = serial.Serial()
+
+    port_id = ""
+    baud_rate = 57600
+    timeout = 1
+    command = str()
+    callback = None
+
+    def __init__(self, port_id, cb):
+        """
+        Constructor:
+        @param command_deque: A reference to the deque shared with the _command_handler
+        """
+        self.port_id = port_id
+        self.callback = cb
+
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.arduino = serial.Serial(self.port_id, self.baud_rate,
+                                     timeout=int(self.timeout), writeTimeout=0)
+
+        self.stop_event = threading.Event()
+
+        # without this, running python 3.4 is extremely sluggish
+        if sys.platform == 'linux':
+            # noinspection PyUnresolvedReferences
+            self.arduino.nonblocking()
+
+    def stop(self):
+        self.stop_event.set()
+
+    def is_stopped(self):
+        return self.stop_event.is_set()
+
+    def open(self, verbose):
+        """
+        open the serial port using the configuration data
+        returns a reference to this instance
+        """
+        # open a serial port
+        if verbose:
+            print('\nOpening Arduino Serial port %s ' % self.port_id)
+
+        try:
+
+            # in case the port is already open, let's close it and then
+            # reopen it
+            self.arduino.close()
+            time.sleep(1)
+            self.arduino.open()
+            time.sleep(1)
+            return self.arduino
+
+        except Exception:
+            # opened failed - will report back to caller
+            raise
+
+    def close(self):
+        """
+            Close the serial port
+            return: None
+        """
+        try:
+            self.arduino.close()
+        except OSError:
+            pass
+
+    def write(self, data):
+        """
+            write the data to the serial port
+            return: None
+        """
+        if sys.version_info[0] < 3:
+            self.arduino.write(data)
+        else:
+            self.arduino.write(bytes([ord(data)]))
+
+    # noinspection PyExceptClausesOrder
+    def run(self):
+        """
+        This method continually runs. If an incoming character is available on the serial port
+        it is read and placed on the _command_deque
+        @return: Never Returns
+        """
+        while not self.is_stopped():
+            # we can get an OSError: [Errno9] Bad file descriptor when shutting down
+            # just ignore it
+            try:
+                if self.arduino.inWaiting():
+                    c = self.arduino.read()
+                    self.command += str(c)
+                    if c=='\n':
+                        self.callback(self.command)
+                        self.command = str()
+                    if len(self.command)>200:
+                        self.command = str()
+                else:
+                    time.sleep(.1)
+            except OSError:
+                pass
+            except IOError:
+                self.stop()
+        self.close()
---
+++ Python/handleArduheat.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+"""
+Copyright (c) 2013-2015 Alan Yorinks All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU  General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+This file demonstrates how to use some of the basic PyMata operations.
+"""
+
+import time
+import sys
+import signal
+import json
+import os.path
+import csv
+
+from bridgeSerial import bridgeSerial
+
+class handleArduheat():
+    ext = '.CSV'
+    count = 0
+    LogFile = str()
+    printHeader = True
+    logging = False
+    date = str()
+    arduino = None
+    
+    def __init__(self, SerialName, Folder):
+        self.folder = Folder
+        self.ConfFile = self.folder+"SYSTEM.CFG"
+        self.LogFile = self.getLogName()
+        self.printHeader = True
+        self.logging = False
+        self.date = str()
+        self.count = 0
+        self.arduino = bridgeSerial(SerialName,self.processJsonString)
+        
+        self.arduino.open(True)
+        self.arduino.start()
+        
+    def close(self):
+        self.arduino.stop()
+        self.arduino.close()
+        
+    def getLogName(self):
+        return (self.folder+ time.strftime("%Y%m%d")[2:] + str(self.count).zfill(2) + self.ext)
+        
+    def getConf(self,objarray):
+        with open(self.ConfFile+'_','r') as cfgFile:
+            lines = cfgFile.readlines()
+            for line in lines:
+                cfg = json.loads(line)
+                for obj in objarray:
+                    if cfg.keys()==obj.keys():
+                        print("Success!\n")
+                        self.arduino.write(json.dumps(cfg,separators=(',', ':'))+ '\n')
+    
+    def setConf(self,objarray):
+        #Clear and write new config file
+        if os.path.isfile(self.ConfFile):
+            os.remove(self.ConfFile)
+        with open(self.ConfFile,'w') as cfgFile:
+            for obj in objarray:
+                cfgFile.write(json.dumps(obj,separators=(',', ':')))
+                cfgFile.write('\n')
+    
+    def writeLog(self,objarray):
+        #Create new logfile, if new day or if not logging
+        if not (self.date == time.strftime("%d")):
+            self.date = time.strftime("%d")
+            self.logging = False
+        if not self.logging:
+            self.count = 0
+            while os.path.isfile(self.getLogName()):
+                self.count += 1
+            self.logging = True
+            self.printHeader = True
+        with open(self.getLogName(),'a') as csvFile:
+            writedata = csv.writer(csvFile)
+            Headers = ["Time"]
+            Datas = [time.strftime("%Y/%m/%d %H:%M:%S")]
+            for obj in objarray:
+                for attribute, values in obj.items():
+                    idx = 0
+                    if isinstance(values, list):
+                        for value in values:
+                            Headers.append(attribute+str(idx))
+                            Datas.append(value)
+                            idx+=1
+                    else:
+                        Headers.append(attribute)
+                        Datas.append(values)
+            if self.printHeader:
+                writedata.writerow(Headers)
+                self.printHeader = False
+            writedata.writerow(Datas)
+    
+    def processJsonString(self,jstr):
+        print(jstr)
+        var = json.loads(jstr)
+        if 'log' in var:
+            self.writeLog(var['log'])
+        if 'setConf' in var:
+            self.setConf(var['setConf'])
+        if 'getConf' in var:
+            self.getConf(var['getConf'])
+
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C')
+    arduino.close()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)   
+
+#json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
+json_string = '{"getConf": [{"RTypes": [4, 4, 4, 4, 0, 0, 0, 2, 2, 2, 1, 1, 3, 4, 4, 2], "RTs": [20.0, 20.0, 19.0, 22.0, 15.0], "SetType": 0}]}'
+#json_string = '{"getConf":[{"Bcm":5.00}]}'
+json_string += '\n'
+
+arduHeat = handleArduheat('/dev/ttyATH0','/mnt/sda1/')
+arduHeat.arduino.write(json_string)
+#arduHeat.arduino.write(json_string)
+
+
+time.sleep(10)
+arduHeat.close()
