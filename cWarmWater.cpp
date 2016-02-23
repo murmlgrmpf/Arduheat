5504a395fc4ec703faa16ca9301e1ae4b727c726
Thomas Emmert <t.3mmert@gmail.com>
2016-02-23 21:33:16 +0100

    added timee optimal control and good pid parameters

--- cWarmWater.cpp
+++ cWarmWater.cpp
@@ -4,10 +4,19 @@ void cWarmWater::Control(void){
 	// If Warm Water is needed, execute the pump
 	// with the power determined by pid controller.
 	// Else switch pid to manual and stop pump.
-	if(!digitalRead(PinWarmWaterSwitch))
-		Pump.run(SpTemp, IsTemp.get());
+	if(!digitalRead(PinWarmWaterSwitch)){
+            if (SpTemp-5.0 > IsTemp.get()) // Time optimal control
+                Pump.run(1.0);
+            else{
+                if (not(Pump.GetMode())) //  when switching from manual mode
+                    Pump.run(0.20); // add static offset for Power
+            
+                Pump.run(SpTemp, IsTemp.get()); // switch to automatic mode
+            }
+            Period.restart();
+	}
 	else
-		Pump.run();
+		Pump.run(); // manual mode
 	
  	if(Trigger.get())
  		setParam();
@@ -15,12 +24,16 @@ void cWarmWater::Control(void){
 
 void cWarmWater::setParam()
 {
-	double P = min(0.5,max(0.1, 1.127-0.011*IsTempBoilerTop.get()));
-	double D = min(7.0,max(2.6,17.9-0.19*IsTempBoilerTop.get()));
+	// double P = min(0.5,max(0.1, 1.127-0.011*IsTempBoilerTop.get()));
+  double P = 0.079;
+  // double D = min(7.0,max(2.6,17.9-0.19*IsTempBoilerTop.get()));
+  double D = 0.52; 
         // ax+b; 90 == 0.5;  55==1.0;
         // a = (0.5-1.0)/(90-60) = -0.0166; b = 1+0.0166*60 = 2
-        double MaxPower = min(1.0,max(0.5,2-0.0166*IsTempBoilerTop.get()));
-	SpTemp = max(47, 0.143*IsTempBoilerTop.get() + 37.1);
+        // double MaxPower = min(1.0,max(0.5,2-0.0166*IsTempBoilerTop.get()));
+        double MaxPower = 1.0;
+	// SpTemp = max(45, 0.143*IsTempBoilerTop.get() + 37.1);
+  SpTemp = 45; 
 	Pump.SetTunings(P, Pump.GetKi(), D);
         Pump.SetOutputLimits(0.0, MaxPower);
 }
--- cWarmWater.h
+++ cWarmWater.h
@@ -12,12 +12,15 @@
 #include <cPID.h>
 #include <ArduinoJson.h>
 
+#define PumpPeriod 500
+
 class cWarmWater
 {
 	public:
 	cWarmWater(void):
-	Pump(PinPumpWarmWater,0.12, 0.005, 2.8, DIRECT),
-	Trigger(15*60*1000),
+	Pump(PinPumpWarmWater,0.12, 0.0002, 2.8, DIRECT, 0.0, PumpPeriod),
+	Trigger(900000), // 15(min)*60(s/min)*1000(mw/s)
+  Period(86400000), // 1(d)*24(d/h)*60(min/h)*60(s/min)*1000(mw/s)
 	IsTemp(&MPNumSys[0], &MPChanSys[idxTempWarmWater], &SysTempOffset[idxTempWarmWater]),
 	IsTempBoilerTop(&MPNumSys[0], &MPChanSys[idxTempBoilerTop], &SysTempOffset[idxTempBoilerTop]),
 	IsTempWarmWaterToBoiler(&MPNumSys[0], &MPChanSys[idxTempWarmWaterToBoiler], &SysTempOffset[idxTempWarmWaterToBoiler])
@@ -36,7 +39,8 @@ class cWarmWater
 	void getData(JsonObject& root);
 	
 	double SpTemp;
-	
+	cTimer Period;
+  
 	private:
 	void setParam();
 	
