490202e00a8041dd7f66a46dc2f8b486cdd95d64
Thomas Emmert <t.3mmert@googlemail.com>
2021-09-25 11:55:34 +0200

    change completed

--- cHeating.cpp
+++ cHeating.cpp
@@ -82,7 +82,7 @@ void cHeating::checkSinks(void)
 	else if (Pool.shouldCharge()) {
 		Sink = SiChargePool;
 		SpTempSource = Pool.SpTemp();
-		needSource = false;
+		needSource = true;
                 //needSource = false;  //toggle true - false to deactivate charging pool
 	}
 	// Else Charge into boiler anyway
