4d394ebd8b1b35b4085787e63dbafc05db898a61
Thomas Emmert <t.3mmert@googlemail.com>
2021-10-13 14:17:28 +0200

    Mixer Fix 4

--- cHeating.cpp
+++ cHeating.cpp
@@ -159,7 +159,7 @@ void cHeating::selectSink( int Sink )
 	boolean poolMayCharge = false;
 	boolean roomsMayCharge = false;
 	
-	boolean controlByRoomsMixer = (!(TransferStation.GetMode()==AUTOMATIC) || SiCombined);
+	boolean controlByRoomsMixer = (!(TransferStation.GetMode()==AUTOMATIC) || (Boiler.needChargeWarmWater() && Rooms.bAct));
 //	boolean controlByRoomsMixer = !(TransferStation.GetMode()==AUTOMATIC);
 	switch (Sink) {
 		case SiCombined: {
