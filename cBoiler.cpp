32319cf9a18c1c6959aee054f01c047b550e7793
murmlgrmpf <t.3mmert@gmail.com>
2022-12-25 23:35:31 +0100

    refactorings and fixes

--- cBoiler.cpp
+++ cBoiler.cpp
@@ -1,30 +1,23 @@
 #include "cBoiler.h"
 
 cBoiler::cBoiler(cRooms* Rooms_,cWarmWater* WarmWater_):
-Valve(PinValveBoilerOpen,PinValveBoilerClose),
+Valve(PinValveBoiler),
+Pump(PinPumpBoiler, 0.025, 0.0, 0.0, REVERSE),
 TempCharge((&MPNumSys[0]),(&MPChanSys[idxTempBoilerCharge]),(&SysTempOffset[idxTempBoilerCharge])),
-TempReserve1((&MPNumSys[0]),(&MPChanSys[idxTempBoilerReserve1]),(&SysTempOffset[idxTempBoilerReserve1])),
-TempReserve2((&MPNumSys[0]),(&MPChanSys[idxTempBoilerReserve2]),(&SysTempOffset[idxTempBoilerReserve2])),
-TempHead((&MPNumSys[0]),&MPChanSys[idxTempBoilerHead],(&SysTempOffset[idxTempBoilerHead])),
 TempTop((&MPNumSys[0]),&MPChanSys[idxTempBoilerTop],(&SysTempOffset[idxTempBoilerTop])),
-
-Pump(PinPumpBoiler, 0.025, 0.0, 0.0, REVERSE)
+TempHead((&MPNumSys[0]),&MPChanSys[idxTempBoilerHead],(&SysTempOffset[idxTempBoilerHead])),
+TempReserve1((&MPNumSys[0]),(&MPChanSys[idxTempBoilerReserve1]),(&SysTempOffset[idxTempBoilerReserve1])),
+TempReserve2((&MPNumSys[0]),(&MPChanSys[idxTempBoilerReserve2]),(&SysTempOffset[idxTempBoilerReserve2]))
 {
 	Rooms = Rooms_;
 	WarmWater = WarmWater_;
 	Pump.SetOutputLimits(0.0, 1.0);
-	
-	pinMode(PinValveBoiler, OUTPUT);
-	digitalWrite(PinValveBoiler, LOW);
+	bRelPool = false;
 }
 
 void cBoiler::getData( JsonObject& root )
 {
-	//char buffer[30];
-	//PROGMEM prog_char  BoilerValve[]  = "BoilerValve";
-	//strcpy_P(buffer, (char*)pgm_read_word(&(sBoilerValve)));
 	root["BV"] =  static_cast<int>( Valve.get());
-
 	root["BT0"] = TempCharge.getRaw();
 	root["BT1"] = TempTop.get();
 	root["BT2"] = TempHead.get();
@@ -32,10 +25,10 @@ void cBoiler::getData( JsonObject& root )
 	root["BT4"] = TempReserve2.get();
 	
 	root["BP"] =  Pump.get();
-	root["BncW"] =  static_cast<int>( bneedChargeWarmWater);
-	root["BncH"] =  static_cast<int>( bneedChargeHeating);
-	root["Bdisc"] =  static_cast<int>( bDischarging);
-	root["Bc"] =  static_cast<int>( bCharging);
-	root["BTsW"] =  WarmWater->SpTemp;
-	root["BTsc"] =  SpTemp();
+	root["BncW"] = static_cast<int>( bneedChargeWarmWater);
+	root["BncH"] = static_cast<int>( bneedChargeHeating);
+	root["Bdisc"] = static_cast<int>( bDischarging);
+	root["Bc"] = static_cast<int>( bCharging);
+	root["BTsW"] = WarmWater->SpTemp;
+	root["BTsc"] = SpTemp();
 }
--- cBoiler.h
+++ cBoiler.h
@@ -8,11 +8,10 @@
 #include "cValve.h"
 #include "cRooms.h"
 #include "cWarmWater.h"
-#include <cPID.h>
 #include <ArduinoJson.h>
 
 // Charge Margin WarmWater for staged SetPoint
-#define WMargin  13.0
+#define WMargin  12.0
 #define SMargin  4.0
 #define TempSufficientWarmWater 49.0
 
@@ -27,32 +26,33 @@ class cBoiler
 	{
 		double SpTempCharge = 0.0;
 
-    if (needChargeWarmWater())
-      SpTempCharge = 46 + WMargin; // charge WarmWater
-    else if (bneedChargeHeating)
-      SpTempCharge = (Rooms->SpTemp()); // charge Rooms
-    else {
-      SpTempCharge = TempReserve1.get() + SMargin; // Solar Charge
-    }
-		return (SpTempCharge);
+	if (needChargeWarmWater())
+		SpTempCharge = 45 + WMargin; // charge WarmWater
+	else if (bneedChargeHeating)
+		SpTempCharge = (Rooms->SpTemp()); // charge Rooms
+	else
+		SpTempCharge = TempReserve1.get() + SMargin; // Solar Charge
+
+	return (SpTempCharge);
 	}
 	
 	boolean needChargeWarmWater(void)
 	{
-    if (WarmWater->Period.get()) {
-  		// Hysteresis by top and head temperature sensors.
-  		// If top falls below setpoint: charge. If head gets above setpoint: dont charge.
-  		if (TempTop.get() < 50)  bneedChargeWarmWater = true;
-  		if (TempHead.get() > 50) bneedChargeWarmWater = false;
-      }
-    else
-      bneedChargeWarmWater = false;
+	if (WarmWater->Period.get()) {
+		// Hysteresis by top and head temperature sensors.
+		// If top falls below setpoint: charge. If head gets above setpoint: dont charge.
+		if (TempTop.get() < 50)
+			bneedChargeWarmWater = true;
+		if (TempHead.get() > 50)
+			bneedChargeWarmWater = false;
+	}
+	else
+	bneedChargeWarmWater = false;
 		
-		return bneedChargeWarmWater;
+	return bneedChargeWarmWater;
 	}
 	
-	boolean shouldCharge(void)
-	{
+	boolean shouldCharge(void){
 		if (TempTop.get() <= TempSufficientWarmWater)
 			bshouldChargeWarmWater = true;
 		else if (TempHead.get() > TempSufficientWarmWater)
@@ -60,10 +60,9 @@ class cBoiler
 		return bshouldChargeWarmWater;
 	}
 	
-	boolean charge(boolean mayCharge, float TempHeatSource, boolean bneedSink = false)
-	{
+	boolean charge(boolean mayCharge, float TempHeatSource, boolean bneedSink = false){
 		double SpTempCharge = SpTemp();
-    
+
 		boolean need = (needChargeWarmWater() || bneedSink);
 		
 		// if charging is needed, charging is fixed to true	
@@ -73,71 +72,63 @@ class cBoiler
 			bCharging = true;
 		else // Hysteresis for Simultaneous/residual heat charging of boiler
 		{
-			if ((bCharging == false) && (TempHeatSource > (TempReserve1.get() + 3)))
+			if ((bCharging == false) && (TempHeatSource > (TempReserve1.get() + 4)))
 				bCharging = true;
-			if ((bCharging == true) && (TempHeatSource < (TempReserve1.get() + 1)))
+			if ((bCharging == true) && (TempHeatSource < (TempReserve1.get() + 3)))
 				bCharging = false;
 		}
 
 		if (bCharging){
 			double BoilerPumpMin = 0.0;
+
 			if (needChargeWarmWater()) //starting condition for charging of WarmWater
 				BoilerPumpMin = 0.2;
-			else //minimal Pump Power as function of Boiler Temp	
+			else //minimal Pump Power as function of Boiler Temp
 				BoilerPumpMin = max(0.03,((0.1-0.03)/(80-50)*(TempReserve1.get()-50)+0.03)); 
 
 			Pump.SetOutputLimits(BoilerPumpMin, 1.0);
 	
 			if (bneedChargeWarmWater) {
-				TempHeatSource = TempHeatSource + 45;
+				TempHeatSource = TempHeatSource + 45; //Manipulation of TempHeatSource in order to achieve 100% BoilerPump Power
 				SpTempCharge = 56;
 			}
 
 			// Run Pump
 			Pump.run(SpTempCharge, TempHeatSource);
 		}
-		//else // Stop Charging: Stop PID and Pump
-		//Pump.run();
-
-		if (bCharging)
-			digitalWrite(PinValveBoiler, HIGH);
-		else
-			digitalWrite(PinValveBoiler, LOW);
-
 		setValve(); // Open Valve if charge pump running (charging)
 		return bCharging;
 	}
 
-boolean Hot(void)
-{
-	// introduce FlipFlop for Hysteresis of Boiler Discharge
-	if ((bHot == false) && (TempReserve1.get() > (Rooms->SpTemp() + 3)))
-		bHot = true;
-	if ((bHot == true) && (TempReserve1.get() < Rooms->SpTemp()))
-		bHot = false;
-	
-	return bHot;
-}
+boolean Hot(void){
+		// introduce FlipFlop for Hysteresis of Boiler Discharge
+		if ((bHot == false) && (TempReserve1.get() > (Rooms->SpTemp() + 3)))
+			bHot = true;
+		if ((bHot == true) && (TempReserve1.get() < Rooms->SpTemp()))
+			bHot = false;
 
-	void discharge(boolean bNeedSourceBoiler)
-	{
+		return bHot;
+	}
+
+boolean RelPool(void){
+		// introduce Hysteresis for Change of Pool and Boiler charging
+		if (TempHead.get() > 54)
+			bRelPool = true;
+		if (TempTop.get() < 56)
+			bRelPool = false;
+
+	return bRelPool;
+	}
+
+	void discharge(boolean bNeedSourceBoiler){
 		bDischarging = bNeedSourceBoiler;
 		setValve(); // Open Valve if discharging
-		
-		if (bDischarging)
-			digitalWrite(PinValveBoiler, HIGH);
-		else
-			digitalWrite(PinValveBoiler, LOW);
 	}
 	
-	void setValve() {Valve.set(bCharging || bDischarging);
-		
-		if (bCharging || bDischarging)
-			digitalWrite(PinValveBoiler, HIGH);
-		else
-			digitalWrite(PinValveBoiler, LOW);
+	void setValve(){
+		Valve.set(bCharging || bDischarging);
 	}
-
+		
 	void getData(JsonObject& root);
 	
 	cValve Valve;
@@ -147,8 +138,8 @@ boolean Hot(void)
 	cTempSensor TempCharge;
 	cTempSensor TempTop;
 	cTempSensor TempHead;
-	cTempSensor TempReserve2;
 	cTempSensor TempReserve1;
+	cTempSensor TempReserve2;
 	
 	private:
 	cRooms* Rooms;
@@ -162,6 +153,7 @@ boolean Hot(void)
 	boolean bDischarging;
 	boolean bCharging;
 	boolean bHot;
+	boolean bRelPool;
 };
 
 #endif
