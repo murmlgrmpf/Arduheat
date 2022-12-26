25e68d64ad06d7a646e2f5239cd7597bf9bec4c8
murmlgrmpf <t.3mmert@gmail.com>
2022-12-26 09:28:46 +0100

    reformatting

--- cRooms.h
+++ cRooms.h
@@ -29,43 +29,45 @@
 #define dmaxvl 42.0
 
 class cRooms {
- public:
-  boolean bAct;
-  cRooms(cWarmWater* WarmWater_);
+   public:
+	boolean bAct;
+	cRooms(cWarmWater* WarmWater_);
 
-  cTempSensor IsTempHeatingLead;
-  cTempSensor IsTempHeatingReturn;
-  cTempSensor TempOutside;
-  cRoom Room[nRooms] = {{0, this}, {1, this}, {2, this}, {3, this}, {4, this}, {5, this}, {6, this}, {7, this}, {8, this}, {9, this}, {10, this}, {11, this}, {12, this}, {13, this}, {14, this}, {15, this}};
-  cPump Pump;
-  cMixer Mixer;
+	cTempSensor IsTempHeatingLead;
+	cTempSensor IsTempHeatingReturn;
+	cTempSensor TempOutside;
+	cRoom Room[nRooms] = {{0, this},  {1, this},  {2, this},  {3, this}, {4, this},  {5, this},
+						  {6, this},  {7, this},  {8, this},  {9, this}, {10, this}, {11, this},
+						  {12, this}, {13, this}, {14, this}, {15, this}};
+	cPump Pump;
+	cMixer Mixer;
 
-  double getNeed(void);
-  boolean active(void);
-  double SpTemp(void);
-  SetTypes SetType;
+	double getNeed(void);
+	boolean active(void);
+	double SpTemp(void);
+	SetTypes SetType;
 
-  void charge(boolean bneedChargeRooms, boolean bcloseMixer = false);
+	void charge(boolean bneedChargeRooms, boolean bcloseMixer = false);
 
-  void initDefaultSetpoint();
-  double MasterSpTemps[nRoomTypes];
-  sTempSchedule TempOffsetSchedule[nSetTypes][nRoomTypes][nDayTypes][nSwitch];
+	void initDefaultSetpoint();
+	double MasterSpTemps[nRoomTypes];
+	sTempSchedule TempOffsetSchedule[nSetTypes][nRoomTypes][nDayTypes][nSwitch];
 
-  void getOffsetTime(JsonObject& root);
-  int setOffsetTime(JsonObject& root);
-  void getOffsetTemp(JsonObject& root);
-  int setOffsetTemp(JsonObject& root);
-  void getRooms(JsonObject& root);
-  int setRooms(JsonObject& root);
+	void getOffsetTime(JsonObject& root);
+	int setOffsetTime(JsonObject& root);
+	void getOffsetTemp(JsonObject& root);
+	int setOffsetTemp(JsonObject& root);
+	void getRooms(JsonObject& root);
+	int setRooms(JsonObject& root);
 
-  void getData(JsonObject& root);
+	void getData(JsonObject& root);
 
-  cLFPWM PWM;
-  cWarmWater* WarmWater;
+	cLFPWM PWM;
+	cWarmWater* WarmWater;
 
- private:
-  double dMaxSp;
-  double MaxNeed;
+   private:
+	double dMaxSp;
+	double MaxNeed;
 };
 
 #endif
