eb958907887908c7e7b4367d531eb89ce7315bc9
murmlgrmpf <t.3mmert@gmail.com>
2022-12-26 12:43:14 +0100

    fix indent

--- Arduheat.ino
+++ Arduheat.ino
@@ -33,39 +33,38 @@ cConfig Config(&Serial,&Heating);
 * @fn void setup(void)
 * @brief Initializes the serial connection and defines the in- and outputs.
 */
-void  setup()
-{
-    // Initialize the Bridge, Console and FileSystem
-    Serial.begin(57600); // Set the baud.
-    while (!Serial) {} // Wait until Serial1 is ready
-    
-    // RTC
-    Wire.begin();
-    rtc.begin();
-    ////////////////////////
-    //// following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    TimeNow = rtc.now();
+void setup() {
+	// Initialize the Bridge, Console and FileSystem
+	Serial.begin(57600);  // Set the baud.
+	while (!Serial) {
+	}  // Wait until Serial1 is ready
 
-    //analogReference(DEFAULT);
-    analogReference(EXTERNAL);
-    analogRead (0);
-    
-    Config.resetConf();
+	// RTC
+	Wire.begin();
+	rtc.begin();
+	////////////////////////
+	//// following line sets the RTC to the date & time this sketch was compiled
+	rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+	TimeNow = rtc.now();
+
+	// analogReference(DEFAULT);
+	analogReference(EXTERNAL);
+	analogRead(0);
+
+	Config.resetConf();
 }
 
 /**
 * @fn void loop(void)
 * @brief Runs the main program which controls the system.
 */
-void loop()
-{
+void loop() {
 	Heating.Control();
 	Heating.WarmWater.Control();
-        Config.updateConf();
-        
+	Config.updateConf();
+
 	if (trigger.get()) {
-                Config.sendLog();
-                TimeNow = rtc.now();
+		Config.sendLog();
+		TimeNow = rtc.now();
 	}
 }
