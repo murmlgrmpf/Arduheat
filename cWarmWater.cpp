dc12c5c484a225f58906bdc536ebb9e0b3c163b2
murmlgrmpf <t.3mmert@gmail.com>
2022-12-26 12:48:59 +0100

    fix indent

--- cWarmWater.cpp
+++ cWarmWater.cpp
@@ -1,56 +1,55 @@
 #include "cWarmWater.h"
 
-void cWarmWater::Control(void){
-	// If Warm Water is needed, execute the pump with the static power plus power determined by PID controller.
-	// Else switch PID to manual and stop pump.
-	WarmWaterFlow = analogRead(PinWarmWaterFlow)/1023.0*Vcc*76/6-26/6; // WarmWaterFlow determined by Sensor converted to Liters/min
+void cWarmWater::Control(void) {
+	// If Warm Water is needed, execute the pump with the static power plus power determined by PID
+	// controller. Else switch PID to manual and stop pump.
+	WarmWaterFlow = analogRead(PinWarmWaterFlow) / 1023.0 * Vcc * 76 / 6 -
+					26 / 6;  // WarmWaterFlow determined by Sensor converted to Liters/min
 
+	double Factor1 = (0.6 - 1) / (75 - 50) * (IsTempBoilerTop.get() - 50) +
+					 1;  // Consideration of Heat Exchanger Behaviour at higher Boiler Temperatures
+	double Factor2 = (1.6 - 1) / (75 - 50) * (IsTempBoilerTop.get() - 50) + 1;
 
-		double Factor1 = (0.6-1)/(75-50)*(IsTempBoilerTop.get()-50)+1; // Consideration of Heat Exchanger Behaviour at higher Boiler Temperatures
-		double Factor2 = (1.6-1)/(75-50)*(IsTempBoilerTop.get()-50)+1;
+	double Kwp =
+		Factor1 * ((Factor2 * 0.025 - 0.0139) / (15 - 2.6) * (WarmWaterFlow - 2.6) +
+				   0.0139);  // Adaption of Pump Power to required primary Flow by PowerStatic value
 
-	double Kwp = Factor1*((Factor2*0.025-0.0139)/(15-2.6)*(WarmWaterFlow-2.6)+0.0139); // Adaption of Pump Power to required primary Flow by PowerStatic value
-
-
-	if (WarmWaterFlow > 2.0){
+	if (WarmWaterFlow > 2.0) {
 		// Modeling of Heat exchanger with proportional correction
-		PowerStatic = Kwp*WarmWaterFlow*((SpTemp+11.5*min(2.2,(SpTemp-IsTemp.getRaw())))-IsTempWaterFromOutside.getRaw())/((max(30,IsTempWarmWaterFromBoiler.getRaw()))-19);
-		Pump.run(SpTemp, max(45.0,IsTemp.getRaw()), PowerStatic);
+		PowerStatic = Kwp * WarmWaterFlow *
+					  ((SpTemp + 11.5 * min(2.2, (SpTemp - IsTemp.getRaw()))) -
+					   IsTempWaterFromOutside.getRaw()) /
+					  ((max(30, IsTempWarmWaterFromBoiler.getRaw())) - 19);
+		Pump.run(SpTemp, max(45.0, IsTemp.getRaw()), PowerStatic);
 		Period.restart();
-	}
-	else{
-	Pump.run(); // manual mode (stop pump)
-	PowerStatic = 0;	
+	} else {
+		Pump.run();  // manual mode (stop pump)
+		PowerStatic = 0;
 	}
 }
 
-void cWarmWater::getSP( JsonObject& root )
-{
-	root["WTs"] = SpTemp;
-}
+void cWarmWater::getSP(JsonObject& root) { root["WTs"] = SpTemp; }
 
-int cWarmWater::setSP( JsonObject& root )
-{
-	if(root.containsKey("WTs")) {
-		if(root["WTs"].is<double>()) {
+int cWarmWater::setSP(JsonObject& root) {
+	if (root.containsKey("WTs")) {
+		if (root["WTs"].is<double>()) {
 			double SpTemp_ = root["WTs"].as<double>();
-		if ((SpTemp_>40)&&(SpTemp_<60)&&(SpTemp_!=SpTemp)) {
-			SpTemp = SpTemp_;
-		return true;
-	}
+			if ((SpTemp_ > 40) && (SpTemp_ < 60) && (SpTemp_ != SpTemp)) {
+				SpTemp = SpTemp_;
+				return true;
+			}
 		}
 	}
-	
+
 	return false;
 }
 
-void cWarmWater::getData( JsonObject& root )
-{
+void cWarmWater::getData(JsonObject& root) {
 	root["WP"] = Pump.get();
 	root["WTi"] = IsTemp.getRaw();
 	root["Wn"] = WarmWaterFlow;
 	root["WS"] = PowerStatic;
-	root["WG"] = Pump.get()+PowerStatic;		
+	root["WG"] = Pump.get() + PowerStatic;
 	root["WTifromB"] = IsTempWarmWaterFromBoiler.getRaw();
 	root["WTifromO"] = IsTempWaterFromOutside.getRaw();
 }
--- cWarmWater.h
+++ cWarmWater.h
@@ -3,43 +3,44 @@
 #ifndef _CWARMWATER_h
 #define _CWARMWATER_h
 
-#include "Arduino.h"
-
-#include "cTemp.h"
-#include "cPump.h"
-#include "PinDefinitions.h"
-#include <cPID.h>
 #include <ArduinoJson.h>
+#include <cPID.h>
 
-class cWarmWater
-{
-	public:
-	cWarmWater(void):
-	Period(86400000), // 1(d)*24(d/h)*60(min/h)*60(s/min)*1000(mw/s)
-	Pump(PinPumpWarmWater,0.0, 0.0, 0.0, DIRECT),
-	IsTemp(&MPNumSys[0], &MPChanSys[idxTempWarmWater], &SysTempOffset[idxTempWarmWater]),
-	IsTempBoilerTop(&MPNumSys[0], &MPChanSys[idxTempBoilerTop], &SysTempOffset[idxTempBoilerTop]),
-	IsTempWarmWaterFromBoiler(&MPNumSys[0], &MPChanSys[idxTempWarmWaterFromBoiler], &SysTempOffset[idxTempWarmWaterFromBoiler]),
-	IsTempWaterFromOutside(&MPNumSys[0], &MPChanSys[idxTempWaterFromOutside], &SysTempOffset[idxTempWaterFromOutside])
-	{
-		PowerStatic = 0;	
+#include "Arduino.h"
+#include "PinDefinitions.h"
+#include "cPump.h"
+#include "cTemp.h"
+
+class cWarmWater {
+   public:
+	cWarmWater(void)
+		: Period(86400000),  // 1(d)*24(d/h)*60(min/h)*60(s/min)*1000(mw/s)
+		  Pump(PinPumpWarmWater, 0.0, 0.0, 0.0, DIRECT),
+		  IsTemp(&MPNumSys[0], &MPChanSys[idxTempWarmWater], &SysTempOffset[idxTempWarmWater]),
+		  IsTempBoilerTop(&MPNumSys[0], &MPChanSys[idxTempBoilerTop],
+						  &SysTempOffset[idxTempBoilerTop]),
+		  IsTempWarmWaterFromBoiler(&MPNumSys[0], &MPChanSys[idxTempWarmWaterFromBoiler],
+									&SysTempOffset[idxTempWarmWaterFromBoiler]),
+		  IsTempWaterFromOutside(&MPNumSys[0], &MPChanSys[idxTempWaterFromOutside],
+								 &SysTempOffset[idxTempWaterFromOutside]) {
+		PowerStatic = 0;
 		pinMode(PinWarmWaterFlow, INPUT);
 		WarmWaterFlow = 0;
 		// Initialize PID controllers for pump
-			Pump.SetOutputLimits(-1.0, 1.0);
-			SpTemp = 45;
+		Pump.SetOutputLimits(-1.0, 1.0);
+		SpTemp = 45;
 	}
-	
+
 	void Control(void);
-	
+
 	void getSP(JsonObject& root);
 	int setSP(JsonObject& root);
 	void getData(JsonObject& root);
-	
+
 	double SpTemp;
 	cTimer Period;
-	
-	private:
+
+   private:
 	float WarmWaterFlow;
 	double PowerStatic;
 	cPump Pump;
@@ -49,6 +50,4 @@ class cWarmWater
 	cTempSensor IsTempWaterFromOutside;
 };
 
-
 #endif
-
