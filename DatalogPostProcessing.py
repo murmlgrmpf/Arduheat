ed48362e59946450281bf113e9d2cba6c0b17ed6
Thomas Emmert <t.3mmert@googlemail.com>
2025-05-25 12:34:35 +0200

    update postprocessing

--- DatalogPostProcessing.py
+++ DatalogPostProcessing.py
@@ -16,9 +16,7 @@ plt.close('all')
 ###############Select Plots##############
 plotrooms = True
 ##############Select File################
-Logfile = "Logs/19041901.CSV"
-#Logfile = "//DISKSTATION/Installation/Windows Programs/_Programs Heating7Arduino/Logs/15042300.CSV"
-
+Logfile = "23112400.CSV"
 with open(Logfile) as f:
     readdata = csv.reader(f)
 
@@ -40,8 +38,8 @@ def plotId(id):
 plotId.i = 0
 
 
-def plotIdax(ax,id):
-    ax.plot(time, data[:,Header.index(id)],'--', label = id, marker= 'o', markersize=7, markevery=(10*plotIdax.i,60))
+def plotIdax(ax,id,factor=1):
+    ax.plot(time, data[:,Header.index(id)]*factor,'--', label = id, marker= 'o', markersize=7, markevery=(10*plotIdax.i,60))
     plotIdax.i+=1
 plotIdax.i = 0
 
@@ -77,28 +75,29 @@ time = np.array(itime)
 if plotrooms:
     plt.figure()
     for n in range(0,16):
-        plt.plot(time, data[:,2+n], label = Header[2+n])
-    plt.legend()
+        plt.plot(time, data[:,n], label = Header[n])
+#        plt.axhline(y = 0.6)
+        plt.legend(loc='upper left')
     
     plt.figure()
     for n in range(0,16):
-        plt.plot(time, data[:,18+n], label = Header[18+n])
-    plt.legend()
+        plt.plot(time, data[:,16+n], label = Header[16+n])
+        plt.legend(loc='upper left')
     
     plt.figure()
     for n in range(0,16):
-        plt.plot(time, data[:,35+n], label = Header[35+n])
-    plt.legend()
+        plt.plot(time, data[:,32+n], label = Header[32+n])
+        plt.legend(loc='upper left')
     
     for n in range(0,16):
         plt.figure()
-        plt.plot(time, data[:,2+n], label = Header[2+n])
-        plt.plot(time, data[:,18+n], label = Header[18+n])
-        plt.plot(time, data[:,35+n], label = Header[35+n])
+        plt.plot(time, data[:,n], label = Header[n])
+        plt.plot(time, data[:,16+n], label = Header[16+n])
+        plt.plot(time, data[:,32+n], label = Header[32+n])
         
         plotId('RTitoR')
         plotId('RTitoSys')
-        plt.legend()
+        plt.legend(loc='upper left')
         
 OverrideOffsets = data[:data.shape[0]-3,17:33]
 
@@ -107,30 +106,25 @@ for column in OverrideOffsets.T:
     c = column.mean()
     average.append(20-c)
 
-
-# Plot system
-#plt.figure()
-#plotId('Toutside')
-
-# PID
-
 plt.figure().suptitle('Warm water')
 ax1 = plt.subplot(2,1,1)
 plotIdax(ax1,'WTi')
 plotIdax(ax1,'WTifromB')
 plotIdax(ax1,'BT1')
-plotIdax(ax1,'WTitoB')
+plotIdax(ax1,'WTifromO')
+plt.axhline(y = 45)
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 ax2 = plt.subplot(2,1,2, sharex=ax1)
-plotIdax(ax2,'WS')
 plotIdax(ax2,'WG')
-#plotIdax(ax2,'WP')
+plotIdax(ax2,'WS')
+
 id1 = 'Wn'
 e = data[:,Header.index(id1)]/20
 ax2.plot(time, e, label = 'Wn = l/min / 20')
 ax2.set_ylim(0,1.0)
 plt.legend(); plt.gca().xaxis.grid(True)
-#plotIdax(ax2,'Wn')
+plt.legend(loc='upper left')
 
 plt.figure().suptitle('Rooms Pump')
 ax1 = plt.subplot(2,1,1)
@@ -141,11 +135,13 @@ e = np.subtract(data[:,Header.index(id1)], data[:,Header.index(id2)])-5
 ax1.plot(time, e, label =  ('e', id3))
 ax1.plot(time,data[:,Header.index(id3)], label = (id3))
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 ax2 = plt.subplot(2,1,2, sharex=ax1)
 plotIdax(ax2,'BncW')
 plotIdax(ax2,'Rn')
 plotIdax(ax2,'Tssuht')
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 
 plt.figure().suptitle('Mixer')
 ax1 = plt.subplot(2,1,1)
@@ -157,27 +153,28 @@ e = np.subtract(data[:,Header.index(id1)], data[:,Header.index(id2)])
 ax1.plot(time, e, label = 'e Mischer')
 ax1.plot(time,data[:,Header.index(id3)], label = (id3))
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 ax2 = plt.subplot(2,1,2, sharex=ax1)
-#plotIdax(ax2,'BncW')
-#plotIdax(ax2,'BncH')
-#plotIdax(ax2,'BuBurns')
 plotIdax(ax2,'Rn')
 plotIdax(ax2,'RTitoR')
 plotIdax(ax2,'RTsHeating')
 plotIdax(ax2,'RTitoSys')
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 
 plt.figure().suptitle('Transfer Station')
 ax1 = plt.subplot(2,1,1)
-plotIdax(ax1,'TsTtoTele')
+#plotIdax(ax1,'TsTtoTele')
 plotIdax(ax1,'TsTfromTele')
 plotIdax(ax1,'TsTOp')
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 ax2 = plt.subplot(2,1,2, sharex=ax1)
 plotIdax(ax2,'Tssuht')
 plotIdax(ax2,'TsV')
 plotIdax(ax2,'TsPower')
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 
 plt.figure().suptitle('Boiler')
 ax1 = plt.subplot(2,1,1)
@@ -190,24 +187,28 @@ plotIdax(ax1,'BT4')
 plotIdax(ax1,'TsTOp')
 plotIdax(ax1,'STtoSys')
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 ax2 = plt.subplot(2,1,2, sharex=ax1)
 plotIdax(ax2,'BP')
 plotIdax(ax2,'Bdisc')
 plotIdax(ax2,'Bc')
-plt.legend()
+plt.legend(loc='upper left')
 
 plt.figure().suptitle('Solar')
 ax1 = plt.subplot(2,1,1)
 plotIdax(ax1,'STtoSys')
 plotIdax(ax1,'STfromCol')
 plotIdax(ax1,'Toutside')
+plotIdax(ax1,'TPool')
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
 ax2 = plt.subplot(2,1,2, sharex=ax1)
 plotIdax(ax2,'SP')
 plotIdax(ax2,'SV')
 plotIdax(ax2,'Ssuht')
 plotIdax(ax2,'Sprobing')
-id1 = 'Sint'
-e = data[:,Header.index(id1)]/1000
-ax2.plot(time, e, label = 'Sint')
+plotIdax(ax2,'bFeedPool',0.2)
+plotIdax(ax2,'bPoolActive',0.1)
+plotIdax(ax2,'Sint',0.001)
 plt.legend(); plt.gca().xaxis.grid(True)
+plt.legend(loc='upper left')
